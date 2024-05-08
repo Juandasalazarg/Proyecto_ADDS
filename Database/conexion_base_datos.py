2729189111da66a34b158a4dd2f6ec41ef0be907
Juandasalazarg <juandasalazargonzalez@gmail.com>
2024-05-07 19:47:55 -0500

    commit

--- Database/conexion_base_datos.py
+++ Database/conexion_base_datos.py
@@ -1,6 +1,6 @@
 import psycopg2
 
-def agregar_usuario(id, nombre, usuario, contraseña, email):
+def agregar_usuario(nombre, usuario, contrasena, email):
     try:
         connection = psycopg2.connect(
             host='localhost',
@@ -11,48 +11,8 @@ def agregar_usuario(id, nombre, usuario, contraseña, email):
 
         cursor = connection.cursor()
 
-        cursor.execute("INSERT INTO usuario (id, nombre, usuario, contraseña, email) VALUES (%s, %s, %s, %s, %s)",
-                       (id, nombre, usuario, contraseña, email))
-        connection.commit()
-
-    finally:
-        cursor.close()
-        connection.close()
-
-
-
-def borrar_usuario(id):
-    try:
-        connection = psycopg2.connect(
-            host='localhost',
-            user='postgres',
-            password='123456789',
-            database='Proyecto_ADDS'
-        )
-
-        cursor = connection.cursor()
-
-        cursor.execute("DELETE FROM Usuario WHERE id = %s", (id,))
-        connection.commit()
-
-    finally:
-        cursor.close()
-        connection.close()
-
-
-def actualizar_usuario(id, nombre, usuario, contraseña, email):
-    try:
-        connection = psycopg2.connect(
-            host='localhost',
-            user='postgres',
-            password='123456789',
-            database='Proyecto_ADDS'
-        )
-
-        cursor = connection.cursor()
-
-        cursor.execute("UPDATE Usuario SET nombre = %s, usuario = %s, contraseña = %s, email = %s WHERE id = %s",
-                       (nombre, usuario, contraseña, email, id))
+        cursor.execute("INSERT INTO usuario (nombre, usuario, contraseña, email) VALUES (%s, %s, %s, %s)",
+                       (nombre, usuario, contrasena, email))
         connection.commit()
 
     finally:
@@ -62,7 +22,7 @@ def actualizar_usuario(id, nombre, usuario, contraseña, email):
 
 
 
-def consultar_usuario(id):
+def borrar_usuario(usuario, contrasena):
     try:
         connection = psycopg2.connect(
             host='localhost',
@@ -73,16 +33,59 @@ def consultar_usuario(id):
 
         cursor = connection.cursor()
 
-        cursor.execute("SELECT * FROM Usuario WHERE id = %s", (id,))
+        cursor.execute("DELETE FROM Usuario WHERE usuario = %s AND contraseña = %s", (usuario, contrasena))
+        connection.commit()
+
+    finally:
+        cursor.close()
+        connection.close()
+
+
+
+
+
+def actualizar_usuario(usuario, nombre, nuevo_usuario, nueva_contrasena, nuevo_email):
+    try:
+        connection = psycopg2.connect(
+            host='localhost',
+            user='postgres',
+            password='123456789',
+            database='Proyecto_ADDS'
+        )
+
+        cursor = connection.cursor()
+
+        cursor.execute("UPDATE Usuario SET nombre = %s, usuario = %s, contraseña = %s, email = %s WHERE usuario = %s",
+                       (nombre, nuevo_usuario, nueva_contrasena, nuevo_email, usuario))
+        connection.commit()
+
+    finally:
+        cursor.close()
+        connection.close()
+
+
+
+
+
+def consultar_usuario(usuario):
+    try:
+        connection = psycopg2.connect(
+            host='localhost',
+            user='postgres',
+            password='123456789',
+            database='Proyecto_ADDS'
+        )
+
+        cursor = connection.cursor()
+
+        cursor.execute("SELECT * FROM Usuario WHERE usuario = %s", (usuario,))
         user_data = cursor.fetchone()
 
         if user_data:
             return user_data  # Devuelve la información del usuario como una tupla
         else:
-            return None  # Retorna None si no se encuentra ningún usuario con ese id
+            return None  # Retorna None si no se encuentra ningún usuario con ese nombre de usuario
 
     finally:
         cursor.close()
         connection.close()
-
-
--- Database/database.sql
+++ Database/database.sql
@@ -1,6 +1,6 @@
 CREATE TABLE Usuario (
-    id VARCHAR(20) PRIMARY KEY,
-    nombre VARCHAR(50),
+    id SERIAL,
+    nombre VARCHAR(50) ,
     usuario VARCHAR(50) UNIQUE,
     contraseña VARCHAR(15),
     email VARCHAR(30) UNIQUE
